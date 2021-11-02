1009f2792cdfd1cbddd490590fe0b25615c78174
Fabio Angelici <brosandi@hotmail.it>
2021-11-02 15:50:12 +0100

    Aggiunto recupero citazioni da API, condivisione su twitter e aggiunto loader

--- main.js
+++ main.js
@@ -1,1 +1,65 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
+let apiQuotes = [];
+
+// Show Loading
+const loading = () => {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+};
+
+// Hide Loading
+const completeLoading = () => {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+};
+
+// Show New Quote
+const newQuote = () => {
+    loading();
+    // Pick a random quote from apiQuotes array
+    const apiQuotesIndex = Math.floor(Math.random() * apiQuotes.length);
+    const quote = apiQuotes[apiQuotesIndex];
+    // Check if Author field is blank and replace it with 'Unknown'
+    !quote.author
+        ? (authorText.textContent = 'Unknown')
+        : (authorText.textContent = quote.author);
+    // Check quote length to determine the styling
+    quote.text.length > 120
+        ? quoteText.classList.add('long-quote')
+        : quoteText.classList.remove('long-quote');
+    // Set Quote, Hide Loader
+    quoteText.textContent = quote.text;
+    completeLoading();
+};
+
+// Tweet Quote
+const tweetQuote = () => {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+};
+
+// Get quotes from API
+async function getQuotes() {
+    loading();
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        alert('An error occured, please try later!');
+    }
+}
+
+// Event Listener
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// On Load
+getQuotes();
